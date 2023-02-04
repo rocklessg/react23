e41ca95406aa346df05e6c73187d132234e753e5
rocklessg <abdulhafiz.suleiman@decagon.dev>
2023-02-04 12:33:12 +0100

    Event handler module

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Counter from './components/Counter';
 import FunctionClick from './components/FunctionClick'
 import ClassClick from './components/ClassClick';
 import EventBind from './components/EventBind';
+import ParentComponent from './components/ParentComponent';
 
 // function App() {
 //   return (
@@ -37,7 +38,8 @@ class App extends Component {
   render() {
     return(
       <div className="App">
-        < EventBind />
+        < ParentComponent />
+        {/* < EventBind /> */}
         {/* < FunctionClick />
         < ClassClick /> */}
         {/* < Greet name="Al-amin" heroname="Abdulhafiz" /> */}
--- src/components/EventBind.js
+++ src/components/EventBind.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react'
 
 class EventBind extends Component {
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         message: 'Hello'
+      }
+      // this.clickHandler = this.clickHandler.bind(this) 3
+    }
+    
+    // clickHandler() {
+    //     this.setState({ 
+    //         message: 'GoodBye!' 
+    //     })
+    //     console.log(this)
+    // }
+
+    clickHandler = () => { //4
+      this.setState({
+          message: 'Goodbye!'
+        })
+    }
   render() {
     return (
       <div>
-        
+            <div>{this.state.message}</div>
+            {/* <button onClick={this.clickHandler.bind(this)}>Click</button> 1*/} 
+            {/* <button onClick={() => this.clickHandler()}>Click</button> 2*/}
+            <button onClick={this.clickHandler}>Click</button>
+
       </div>
     )
   }
