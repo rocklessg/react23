fe560174545e0fe6082451799c69cf60cdf34e84
rocklessg <abdulhafiz.suleiman@decagon.dev>
2023-02-07 08:05:01 +0100

    Updating Lifecycle Method

--- src/components/LifecycleA.js
+++ src/components/LifecycleA.js
@@ -20,11 +20,32 @@ class LifecycleA extends Component {
         console.log('LifecycleA componentDidMount')
     }
 
+    shouldComponentUpdate() {
+       console.log('LifecycleA shouldComponentUpdate')
+       return true
+    }
+
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+      console.log('LifecycleA getSnapshotBeforeUpdate')
+      return null
+    }
+
+    componentDidUpdate() {
+      console.log('LifecycleA componentDidUpdate')
+    }
+
+    changeState = () => {
+      this.setState({
+        name: 'Coders Haven'
+      })
+    }
+
   render() {
     console.log('LifecycleA render')
     return (
       <div>
-            LifecycleA
+            <div>LifecycleA</div>
+            <button onClick={this.changeState}>Change State</button>
             < LifecycleB />
       </div>
     )
