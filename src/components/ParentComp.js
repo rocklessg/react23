4888b65c5be019410739866c8a3663a072412eea
rocklessg <abdulhafiz.suleiman@decagon.dev>
2023-02-08 08:00:42 +0100

    Memo for functional component

--- src/components/ParentComp.js
+++ src/components/ParentComp.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import RegularComp from './RegularComp'
 import PureComp from './PureComp'
+import MemoComp from './MemoComp'
 
 class ParentComp extends Component {
     constructor(props) {
@@ -24,8 +25,9 @@ class ParentComp extends Component {
     return (
       <div>
         Parent Component
-        <RegularComp name={this.state.name}></RegularComp>
-        <PureComp name={this.state.name}></PureComp>
+        {/* <RegularComp name={this.state.name}></RegularComp>
+        <PureComp name={this.state.name}></PureComp> */}
+        < MemoComp name={this.state.name}/>
       </div>
     )
   }
